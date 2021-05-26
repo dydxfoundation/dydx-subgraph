b8a0f55b5ae2e3314b17654835990e6bfdb0b014
Lucas <lucas@dydx.exchange>
2021-05-26 15:00:48 -0700

    Don't change user token balance on claim events

---
+++ src/rewards.ts
@@ -0,0 +1,15 @@
+import { Address } from "@graphprotocol/graph-ts"
+import {
+  Claimed,
+} from "../generated/CumulativeMerkleDistributor/CumulativeMerkleDistributor"
+import { User } from "../generated/schema"
+import { getUser } from "./helpers";
+
+export function handleClaimed(event: Claimed): void {
+  let userEthereumAddress: Address = event.params.account;
+  let user: User = getUser(userEthereumAddress)
+
+  user.totalRewardsClaimed = user.totalRewardsClaimed.plus(event.params.cumulativeAmount);
+
+  user.save()
+}
