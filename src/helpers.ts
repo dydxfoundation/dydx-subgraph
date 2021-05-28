a51b01d8d592cda2ed999515a37ada86558eb764
Lucas <lucas@dydx.exchange>
2021-05-28 16:24:19 -0700

    Track voting and proposing power

--- src/helpers.ts
+++ src/helpers.ts
@@ -37,10 +37,8 @@ export function changeUserStakedTokenBalance(address: Address, amount: BigInt, a
 
   if (add) {
     user.totalStakedTokens = user.totalStakedTokens.plus(amount)
-    user.votingPower = user.votingPower.plus(amount)
   } else {
     user.totalStakedTokens = user.totalStakedTokens.minus(amount)
-    user.votingPower = user.votingPower.minus(amount)
   }
 
   user.save()
