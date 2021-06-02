5bea692f0627d2b766ae6645bc26b9eba60ecddd
lucas-dydx <76970939+lucas-dydx@users.noreply.github.com>
2021-06-02 14:51:35 -0700

    Track user staked token balance [BAC-1360] (#2)

--- src/helpers.ts
+++ src/helpers.ts
@@ -7,8 +7,9 @@ export function getUser(address: Address): User {
   let user = User.load(address.toHexString())
   if (!user) {
     user = new User(address.toHexString())
-    user.totalRewardsClaimed = BigInt.fromI32(0)
     user.totalTokens = BigInt.fromI32(0)
+    user.totalStakedTokens = BigInt.fromI32(0)
+    user.votingPower = BigInt.fromI32(0)
   }
 
   return user!;
@@ -19,8 +20,24 @@ export function changeUserTokenBalance(address: Address, amount: BigInt, add: bo
 
   if (add) {
     user.totalTokens = user.totalTokens.plus(amount)
+    user.votingPower = user.votingPower.plus(amount)
   } else {
     user.totalTokens = user.totalTokens.minus(amount)
+    user.votingPower = user.votingPower.minus(amount)
+  }
+
+  user.save()
+}
+
+export function changeUserStakedTokenBalance(address: Address, amount: BigInt, add: boolean): void {
+  let user: User = getUser(address)
+
+  if (add) {
+    user.totalStakedTokens = user.totalStakedTokens.plus(amount)
+    user.votingPower = user.votingPower.plus(amount)
+  } else {
+    user.totalStakedTokens = user.totalStakedTokens.minus(amount)
+    user.votingPower = user.votingPower.minus(amount)
   }
 
   user.save()
