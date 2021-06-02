99ab217a06ce9c4ae213920b31b864950f2255a8
Lucas <lucas@dydx.exchange>
2021-06-02 14:34:49 -0700

    Address PR comments

--- src/helpers.ts
+++ src/helpers.ts
@@ -5,13 +5,13 @@ export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
 export function getUser(address: Address): User {
   let user = User.load(address.toHexString())
-  if (user == null) {
+  if (!user) {
     user = new User(address.toHexString())
     user.totalRewardsClaimed = BigInt.fromI32(0)
     user.totalTokens = BigInt.fromI32(0)
   }
 
-  return user as User;
+  return user!;
 }
 
 export function changeUserTokenBalance(address: Address, amount: BigInt, add: boolean): void {
