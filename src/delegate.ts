fa52237d4fff593531cdad6168800ad45fcdbcbf
Lucas <lucas@dydx.exchange>
2021-06-02 14:23:15 -0700

    Address PR comments

--- src/delegate.ts
+++ src/delegate.ts
@@ -7,11 +7,16 @@ export enum TokenType {
   StakedToken,
 }
 
+enum DelegationType {
+  VotingPower = 0,
+  PropositionPower = 1,
+}
+
 export function handleDelegation(userAddress: Address, amount: BigInt, delegationType: i32, tokenType: TokenType): void {
   let user: User = getUser(userAddress)
 
   let diff: BigInt;
-  if (delegationType == 0) {
+  if (delegationType == DelegationType.VotingPower) {
     // Voting power was delegated
     if (tokenType == TokenType.Token) {
       diff = amount.minus(user.tokenVotingPower)
@@ -25,8 +30,8 @@ export function handleDelegation(userAddress: Address, amount: BigInt, delegatio
       return
     }
     // votingPower is combination of both staked + regular token voting power
-    user.votingPower = user.votingPower.minus(diff)
-  } else if (delegationType == 1) {
+    user.votingPower = user.votingPower.plus(diff)
+  } else if (delegationType == DelegationType.PropositionPower) {
     // Proposition power was delegated
     if (tokenType == TokenType.Token) {
       diff = amount.minus(user.tokenProposingPower)
@@ -40,7 +45,7 @@ export function handleDelegation(userAddress: Address, amount: BigInt, delegatio
       return
     }
     // proposingPower is combination of both staked + regular token voting power
-    user.proposingPower = user.proposingPower.minus(diff)
+    user.proposingPower = user.proposingPower.plus(diff)
   } else {
     // Unknown option
     log.error("Unknown delegation type", [delegationType.toString()])
