27c3f281dd9ca7693208b1171482cc356bfaa3f3
Lucas <lucas@dydx.exchange>
2021-06-03 15:00:31 -0700

    Remove unused import

--- src/proposal.ts
+++ src/proposal.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt, log } from "@graphprotocol/graph-ts"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
 import { getUser, getProposal, getProposalVote } from "./helpers";
 import {
   ProposalCreated,
